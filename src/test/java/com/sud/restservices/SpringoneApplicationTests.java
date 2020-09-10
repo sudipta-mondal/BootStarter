4921064d7caa54d267471abeac4d798b8f407674
sudipto.m <sudipto.m@hotmail.com>
2020-09-10 14:58:21 +0530

    First Commit

---
+++ src/test/java/com/sud/restservices/SpringoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sud.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
